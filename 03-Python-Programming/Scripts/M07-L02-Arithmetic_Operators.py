b0992c84883f564c0795ad398a6edb2fde75c8c6
S Malik <smalik.cybersec@gmail.com>
2025-06-28 11:04:00 +0530

    added M07-L02-Arithmetic_Operators.py

---
+++ 03-Python-Programming/Scripts/M07-L02-Arithmetic_Operators.py
@@ -0,0 +1,8 @@
+# Try these out
+print(f"15 + 7 = {15 + 7}")
+print(f"20 - 9 = {20 - 9}")
+print(f"4 * 6 = {4 * 6}")
+print(f"10 / 4 = {10 / 4}")   # Notice the float result
+print(f"10 // 4 = {10 // 4}") # Notice the integer result
+print(f"17 % 5 = {17 % 5}")   # What's the remainder?
+print(f"3 ** 4 = {3 ** 4}")   # 3 to the power of 4
